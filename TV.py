Fixed gradient descent implementation

TV.py
@@ -4,7 +4,7 @@ import math
 from astra import add_noise_to_sino
 import numeric.radon as r
 
-theta = np.linspace(0, np.pi, 60, False)
+theta = np.linspace(0, np.pi, 180, False)
 
 
 def A(f):
@@ -41,9 +41,8 @@ def tv_grad(f, beta):
     grad_hor_f = grad_hor(f)
     grad_vert_f = grad_vert(f)
 
-    denomi = np.sqrt(grad_hor_f**2 + grad_vert_f**2 + beta)
-    nomi_hor = grad_hor_f / denomi
-    nomi_vert = grad_vert_f / denomi
+    nomi_hor = grad_hor_f / np.sqrt(grad_hor_f**2 + grad_vert_f**2 + beta)
+    nomi_vert = grad_vert_f / np.sqrt(grad_hor_f**2 + grad_vert_f**2 + beta)
 
     return div_hor(nomi_hor) + div_vert(nomi_vert)
 
@@ -53,16 +52,30 @@ def projected_grad_desc_TV(meas, f_size):
     Computes the Total Variation regularised reconstruction for the data using the
     projected gradient descent method.
     '''
-    alpha = 1
+    alpha = 10
     beta = 1e-6
-    lambdak = 1e-6
+    lambdak = 1e-4
     fk = At(meas)
+    grad = (2 * At(A(fk)-meas)) - alpha*tv_grad(fk, beta)
 
-    for i in range(100):
-        grad = (2 * At(A(fk)-meas)) - alpha*tv_grad(fk, beta)
+    for i in range(200):
+        fOld = fk
+        gradOld = grad
+
         fk = fk - lambdak*grad
         fk[fk < 0] = 0
-        #print(np.linalg.norm(fk-r.phantom, 2))
+        grad = (2 * At(A(fk)-meas)) - alpha*tv_grad(fk, beta)
+
+        lambdak = (np.dot(fk.flatten()-fOld.flatten(), fk.flatten()-fOld.flatten())) / \
+            (np.dot(fk.flatten()-fOld.flatten(), grad.flatten()-gradOld.flatten()))
+
+        if i % 10 == 0:
+
+            pylab.figure(44)
+            pylab.gray()
+            pylab.title("Iteration {}".format(i))
+            pylab.imshow(fk)
+            pylab.pause(0.1)
 
     return fk
 
@@ -72,13 +85,14 @@ noisy_data = add_noise_to_sino(data, 1e6)
 
 rec = projected_grad_desc_TV(data, (256, 256))
 
-pylab.gray()
+# pylab.gray()
+# pylab.figure(0)
+# pylab.imshow(At(data))
+# pylab.figure(1)
+# pylab.imshow(noisy_data)
+# pylab.figure(2)
+# pylab.imshow(rec)
 pylab.figure(0)
-pylab.imshow(At(data))
-pylab.figure(1)
-pylab.imshow(noisy_data)
-pylab.figure(2)
-pylab.imshow(rec)
-
+pylab.imshow(r.phantom)
 
 pylab.show()

cg_tikhonov.py
@@ -28,14 +28,23 @@ def grad_descent(data):
     hence we take it as a parameter f_shape and leave computing it outside of
     this function. The matrix g is given by data.
     '''
-    alpha = 0.1
-    lambdak = 1*1e-3
+    lambdak = 1e-6
     # We take the simple backprojection of the data as an initial guess.
     fk = At(data)
+    grad = At(A(fk)-data)
 
-    for _ in range(300):
-        grad = At(A(fk)-data)
+    for _ in range(100):
+        fOld = fk
+        gradOld = grad
+
         fk = fk - lambdak*grad
+        grad = At(A(fk)-data)
+
+        lambdak = (np.dot(fk.flatten()-fOld.flatten(), fk.flatten()-fOld.flatten())) / \
+            (np.dot(fk.flatten()-fOld.flatten(), grad.flatten()-gradOld.flatten()))
+
+        print(lambdak)
+
         fk[fk < 0] = 0
 
     return fk
@@ -49,7 +58,7 @@ def conjugate_gradient_tikhonov(data, f_shape):
     this function. The matrix g is given by data.
     '''
 
-    alpha = 0.15
+    alpha = 0.1
     fk = np.zeros(f_shape)
     grad = Q(fk, alpha) - At(data)
     dk = -grad
@@ -70,8 +79,8 @@ def conjugate_gradient_tikhonov(data, f_shape):
 
 data = r.radon(r.phantom, theta)
 noisy_data = add_noise_to_sino(data, 10000.)
-tik_rec = conjugate_gradient_tikhonov(noisy_data, (256, 256))
-#gd_rec = grad_descent(noisy_data)
+#tik_rec = conjugate_gradient_tikhonov(noisy_data, (256, 256))
+gd_rec = grad_descent(noisy_data)
 bp = r.iradon(noisy_data, theta, 256)
 
 
@@ -81,5 +90,5 @@ pylab.imshow(r.phantom)
 pylab.figure(1)
 pylab.imshow(noisy_data)
 pylab.figure(2)
-pylab.imshow(tik_rec)
+pylab.imshow(gd_rec)
 pylab.show()