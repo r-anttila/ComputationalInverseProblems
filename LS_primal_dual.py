Fixed Tikhonov and radon transform calculation

LS_primal_dual.py
@@ -1,15 +1,14 @@
 import pylab
 import numpy as np
 import math
-from astra import add_noise_to_sino
 from numeric import radon as r, tv_grad as tv
 
-n_ang = 30
+n_ang = 90
 n = 128
 
 phantom = {32:r.phantom32, 64:r.phantom64, 128:r.phantom128, 256:r.phantom}
 
-theta = np.linspace(0, np.pi, n_ang, False)
+theta = np.linspace(0, np.pi/2, n_ang, False)
 
 def Af(f):
     # Define the forward projection with given number of angles
@@ -75,7 +74,7 @@ def LS_primal_dual(meas, f_shape, show_prog=False):
     fk = np.zeros(f_shape)
     ft = fk
 
-    for i in range(1001):
+    for i in range(5001):
         pk = (pk+sigma*(Af(ft) - meas))/(1+sigma)
         f_prev = fk
         fk = fk - tau*Atf(pk)
@@ -86,9 +85,8 @@ def LS_primal_dual(meas, f_shape, show_prog=False):
             if i % 50 == 0:
                 gap = 1/2*np.linalg.norm(Af(fk)-meas)**2 + 1/2 * \
                     np.linalg.norm(pk)**2 + np.dot(pk.flatten(), meas.flatten())
-                at = np.linalg.norm(Atf(pk).flatten(), np.inf)
                 pylab.figure(1)
-                pylab.title("Iteration {} | Dual gap: {:.2f} | A^tp: {:.2f}".format(i, gap, at))
+                pylab.title("Iterations {} | Dual gap: {:.2f}".format(i, gap))
                 pylab.gray()
                 pylab.imshow(fk)
                 pylab.pause(0.1)
@@ -126,9 +124,8 @@ def LS_primal_dual_mat(A, At, meas, f_shape,show_prog=False):
             if i % 50 == 0:
                 gap = 1/2*np.linalg.norm(A.dot(ft)-meas_v, 2)**2 + 1/2 * \
                     np.linalg.norm(pk, 2)**2 + np.dot(pk, meas_v)
-                at = np.linalg.norm(At.dot(pk), np.inf)
                 pylab.figure(2)
-                pylab.title("Iteration {} | Dual gap: {:.2f} | A^tp: {:.2f}".format(i, gap, at))
+                pylab.title("Iteration {} | Dual gap: {:.2f}".format(i, gap))
                 pylab.gray()
                 pylab.imshow(np.reshape(ft,f_shape))
                 pylab.pause(0.1)
@@ -140,16 +137,16 @@ if __name__ == "__main__":
     # Creating measurement data
     data = r.radon(phantom[n], theta)
 
-    #A = r.radon_matrix(n_ang, n)
+    #A = r.radon_matrix(theta, n)
     #At = np.transpose(A)
 
     # Adding noise
-    noise_level = 0.01
+    noise_level = 0.05
     noisy_data = data + noise_level*np.random.randn(data.shape[0], data.shape[1])
 
     # Compute the reconstruction
     rec = LS_primal_dual(noisy_data, (n, n), True)
-    #rec2 = LS_primal_dual_mat(A, At, noisy_data, (n,n), True)
+    #rec = LS_primal_dual_mat(A, At, noisy_data, (n,n), True)
 
 
     pylab.gray()
@@ -157,5 +154,7 @@ if __name__ == "__main__":
     pylab.imshow(phantom[n])
     pylab.figure(2)
     pylab.plot(np.linspace(0,n,n), rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
+
+    print("Error: {}".format(np.linalg.norm(phantom[n]-rec)))
 
     pylab.show()

TV.py
@@ -1,11 +1,10 @@
 import pylab
 import numpy as np
 import math
-from astra import add_noise_to_sino
 from numeric import radon as r, tv_grad as tv
 
-n = 256
-theta = np.linspace(0, np.pi, 60, False)
+n = 128
+theta = np.linspace(0, np.pi, 180, False)
 phantom = {32:r.phantom32, 64:r.phantom64, 128:r.phantom128, 256:r.phantom}
 
 
@@ -28,13 +27,13 @@ def projected_grad_desc_TV(meas, f_size):
     Computes the Total Variation regularised reconstruction for the data using the
     projected gradient descent method.
     '''
-    alpha = 1
-    beta = 1e-8
+    alpha = 0.1
+    beta = 1e-6
     lambdak = 1e-4
     fk = At(meas)
     grad = (2 * At(A(fk)-meas)) - alpha*tv.smoothed_tv_grad(fk, beta)
 
-    for i in range(1001):
+    for i in range(5001):
         fOld = fk
         gradOld = grad
 
@@ -57,7 +56,7 @@ def projected_grad_desc_TV(meas, f_size):
 
 
 data = r.radon(phantom[n], theta)
-noise_level = 0.1
+noise_level = 0.05
 noisy_data = data + noise_level*np.random.randn(data.shape[0], data.shape[1])
 
 rec = projected_grad_desc_TV(noisy_data, (n, n))
@@ -70,6 +69,8 @@ pylab.imshow(noisy_data)
 # pylab.figure(2)
 # pylab.imshow(rec)
 pylab.figure(1)
-pylab.plot(np.linspace(0,n,n), rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
+pylab.plot(np.linspace(0,n,n), phantom[n][n//2,:], 'b',np.linspace(0,n,n), rec[n//2,:], 'r')
+
+print("Error: {}".format(np.linalg.norm(phantom[n]-rec)))
 
 pylab.show()

TV_primal_dual.py
@@ -1,11 +1,10 @@
 import pylab
 import numpy as np
 import math
-from astra import add_noise_to_sino
 from numeric import radon as r, tv_grad as tv
 
-n_ang = 60
-n = 256
+n_ang = 180
+n = 128
 
 phantom = {32:r.phantom32, 64:r.phantom64, 128:r.phantom128, 256:r.phantom}
 
@@ -29,7 +28,7 @@ def norm(f_shape):
 
     xk = np.ones(f_shape)
 
-    for i in range(21):
+    for i in range(41):
         xk = Atf(Af(xk)) - tv.divgrad(xk)
         xk = xk/np.linalg.norm(xk)
 
@@ -41,7 +40,7 @@ def norm_mat(A, At, f_shape):
 
     xk = np.ones(f_shape)
 
-    for i in range(21):
+    for i in range(41):
         xk = At.dot(A.dot(xk.flatten())) - tv.divgrad(np.reshape(xk, f_shape)).flatten()
         xk = xk/np.linalg.norm(xk)
 
@@ -57,7 +56,7 @@ def TV_primal_dual(meas, f_shape, alpha, show_prog=False):
     L = norm(f_shape)
     sigma = 1/L
     tau = 1/L
-    Theta = 0.9
+    Theta = 0.99
     # Initialize the solution pk to the dual problem to zero
     pk = np.zeros(meas.shape)
     # Initialize the solution fk to the primal problem to zero
@@ -68,25 +67,24 @@ def TV_primal_dual(meas, f_shape, alpha, show_prog=False):
     qk_vert = np.zeros(f_shape)
     ft = np.reshape(fk, f_shape)
     
-    for i in range(1001):
+    for i in range(5001):
         pk = (pk+sigma*(Af(ft)-meas))/(1+sigma)
         f_prev = fk
         grad, qk_hor, qk_vert = tv.tv_grad(ft, qk_hor, qk_vert, alpha, sigma)
         fk = fk-tau*Atf(pk) + tau*grad
         fk[fk<0] = 0
         ft = np.reshape(fk + Theta*(fk-f_prev), f_shape)
+
         gap = 1/2*np.linalg.norm(Af(fk)-meas)**2 +alpha*np.sum(np.sqrt(tv.grad_hor(fk)**2+tv.grad_vert(fk)**2)) + 1/2*np.linalg.norm(pk)**2+np.dot(pk.flatten(),meas.flatten())
-        at = np.linalg.norm(Atf(pk).flatten()-grad.flatten(), np.inf)
+
         if show_prog:
             if i % 50 == 0:
                 pylab.figure(2)
-                pylab.title("Iteration {} | Dual gap: {:.2f} | A^tp: {:.2f}".format(i, gap, at))
+                pylab.title("Iteration {} | Dual gap: {:.2f}".format(i, gap))
                 pylab.gray()
                 pylab.imshow(fk)
                 pylab.pause(0.1)
-        
-        if gap <= -20:
-            return ft;
+
 
     return ft
 
@@ -111,21 +109,19 @@ def TV_primal_dual_mat(A, At, meas, f_shape, alpha, show_prog=False):
     ft = np.reshape(fk, f_shape)
     meas_v = meas.flatten()
     
-    for i in range(1001):
+    for i in range(20001):
         pk = (pk+sigma*(A.dot(ft.flatten())-meas_v))/(1+sigma)
         f_prev = fk
         grad, qk_hor, qk_vert = tv.tv_grad(ft, qk_hor, qk_vert, alpha, sigma)
         fk = fk-tau*At.dot(pk.flatten()) + tau*grad.flatten()
-        fk[fk<0] = 0
         ft = np.reshape(fk + Theta*(fk-f_prev), f_shape)
         if show_prog:
             if i % 50 == 0:
                 gap = 1/2*np.linalg.norm(A.dot(ft.flatten())-meas_v)**2 +alpha*np.sum(np.sqrt(tv.grad_hor(ft)**2+tv.grad_vert(ft)**2)) + 1/2*np.linalg.norm(pk)**2+np.dot(pk,meas_v)
-                at = np.linalg.norm(At.dot(pk)-grad.flatten(), np.inf)
                 pylab.figure(2)
-                pylab.title("Iteration {} | Dual gap: {:.2f} | A^tp: {:.2f}".format(i, gap, at))
+                pylab.title("Iteration {} | Dual gap: {}".format(i, gap))
                 pylab.gray()
-                pylab.imshow(ft)
+                pylab.imshow(np.reshape(fk, f_shape))
                 pylab.pause(0.1)
 
     return ft
@@ -135,21 +131,23 @@ if __name__ == "__main__":
     # Creating measurement data
     data = r.radon(phantom[n], theta)
 
-    #A = r.radon_matrix(n_ang, n)
+    #A = r.radon_matrix(theta, n)
     #At = np.transpose(A)
 
     # Adding noise
-    noise_level = 0.1
+    noise_level = 0.05
     noisy_data = data + noise_level*np.random.randn(data.shape[0], data.shape[1])
 
-    #rec = TV_primal_dual_mat(A, At, noisy_data, (n,n), 1, True)
+    #rec = TV_primal_dual_mat(A, At, noisy_data, (n,n), 0.05, True)
 
-    rec = TV_primal_dual(noisy_data, (n,n), 1, True)
+    rec = TV_primal_dual(noisy_data, (n,n), 0.1, True)
 
     pylab.gray()
     pylab.figure(0)
     pylab.imshow(phantom[n])
     pylab.figure(3)
-    pylab.plot(np.linspace(0,n,n), rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
+    pylab.plot(np.linspace(0,n,n), phantom[n][n//2,:], 'b',np.linspace(0,n,n), rec[n//2,:], 'r')
+
+    print("Error: {}".format(np.linalg.norm(phantom[n]-rec)))
 
     pylab.show()

cg_tikhonov.py
@@ -5,7 +5,7 @@ from astra import add_noise_to_sino
 import numeric.radon as r
 
 n = 128
-theta = np.linspace(0, np.pi, 30, False)
+theta = np.linspace(0, np.pi/2, 90, False)
 phantom = {32:r.phantom32, 64:r.phantom64, 128:r.phantom128, 256:r.phantom}
 
 def A(f):
@@ -34,61 +34,37 @@ def grad_descent(data, alpha):
     fk = At(data)
     grad = At(A(fk)-data)+2*alpha*fk
 
-    for _ in range(100):
+    for _ in range(2001):
         fOld = fk
         gradOld = grad
 
         fk = fk - lambdak*grad
+        
         grad = At(A(fk)-data)+2*alpha*fk
 
         lambdak = (np.dot(fk.flatten()-fOld.flatten(), fk.flatten()-fOld.flatten())) / \
             (np.dot(fk.flatten()-fOld.flatten(), grad.flatten()-gradOld.flatten()))
-
-        print(lambdak)
-
         fk[fk < 0] = 0
 
     return fk
 
 
-def conjugate_gradient_tikhonov(data, f_shape):
-    '''
-    This method solves the normal equation (A*A+alphaI)f=A*g iteratively using the
-    conjugate gradient method. We need to know the shape of the desired output
-    hence we take it as a parameter f_shape and leave computing it outside of
-    this function. The matrix g is given by data.
-    '''
-
-    alpha = 0.01
-    fk = np.zeros(f_shape)
-    grad = Q(fk, alpha) - At(data)
-    dk = -grad
-
-    for i in range(math.ceil(1/alpha)):
-        Qdk = Q(dk, alpha)
-        ak = (-np.dot(grad.transpose().flatten(), dk.flatten())) / \
-            (np.dot(dk.transpose().flatten(), Qdk.flatten()))
-        fk = fk + ak*dk
-        prev_grad = grad
-        grad = grad + ak*Qdk
-        bk = (np.dot(grad.transpose().flatten(), grad.flatten())) / \
-            (np.dot(prev_grad.transpose().flatten(), prev_grad.flatten()))
-        dk = bk*dk - grad
-
-    return fk
-
-
 data = r.radon(phantom[n], theta)
-noise_level = 0.01
+noise_level = 0.05
 noisy_data = data + noise_level*np.random.randn(data.shape[0], data.shape[1])
-tik_rec = conjugate_gradient_tikhonov(noisy_data, (n, n))
-#gd_rec = grad_descent(noisy_data, 5)
-bp = r.iradon(noisy_data, theta, n)
+rec = grad_descent(noisy_data, 0.1)
 
-
+'''
 pylab.gray()
 pylab.figure(0)
-pylab.plot(np.linspace(0,n,n), tik_rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
+pylab.plot(np.linspace(0,n,n), rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
 pylab.figure(2)
-pylab.imshow(tik_rec)
+pylab.imshow(rec)
+
+print("Error: {}".format(np.linalg.norm(phantom[n]-rec)))
+'''
+
+fk = At(noisy_data)
+pylab.gray()
+pylab.imshow(fk)
 pylab.show()

numeric/radon.py
@@ -25,18 +25,13 @@ def radon(f, theta):
     f_id = astra.data2d.create('-vol', vol_geom, f)
     sin_id = astra.data2d.create('-sino', proj_geom)
 
-    cfg = astra.astra_dict("FP_CUDA")
-    cfg["ProjectionDataId"] = sin_id
-    cfg["VolumeDataId"] = f_id
+    proj_id = astra.create_projector('line', proj_geom, vol_geom)
 
-    alg_id = astra.algorithm.create(cfg)
-    astra.algorithm.run(alg_id)
-    sin_data = astra.data2d.get(sin_id)
-    astra.algorithm.delete(alg_id)
+    sin_id, sin_data = astra.create_sino(f, proj_id)
 
-    astra.data2d.delete(f_id)
-    astra.data2d.delete(sin_id)
-
+    astra.data2d.clear()
+    astra.projector.clear()
+    
     return sin_data
 
 
@@ -48,21 +43,16 @@ def iradon(g, theta, n):
     vol_geom = astra.create_vol_geom(n)
     proj_geom = astra.create_proj_geom('parallel', 1.0, 367, theta)
 
+    proj_id = astra.create_projector('line', proj_geom, vol_geom)
+
     f_id = astra.data2d.create('-vol', vol_geom)
     g_id = astra.data2d.create('-sino', proj_geom, g)
 
-    cfg = astra.astra_dict("BP_CUDA")
-    cfg["ProjectionDataId"] = g_id
-    cfg["ReconstructionDataId"] = f_id
+    rec_id, rec_data = astra.create_backprojection(g, proj_id)
 
-    alg_id = astra.algorithm.create(cfg)
-    astra.algorithm.run(alg_id)
-    rec_data = astra.data2d.get(f_id)
-    astra.algorithm.delete(alg_id)
-
-    astra.data2d.delete(f_id)
-    astra.data2d.delete(g_id)
-
+    astra.data2d.clear()
+    astra.projector.clear()
+    
     return rec_data
 
 
@@ -93,10 +83,9 @@ def fbp(g, theta, n, filter='ram-lak'):
     return rec_data
 
 
-def radon_matrix(n_angles, M):
+def radon_matrix(angles, M):
     #M is equal to the square root of the image size.
     im_size = M**2
-    angles = np.linspace(0, np.pi, n_angles, False)
     tmp = radon(np.zeros((M,M)), angles)
     k = np.size(tmp)
     l = 0

tik_primal_dual.py
@@ -1,14 +1,13 @@
 import pylab
 import numpy as np
 import math
-from astra import add_noise_to_sino
 from numeric import radon as r, tv_grad as tv
 
-n_ang = 30
-n = 64
+n_ang = 90
+n = 128
 phantom = {32:r.phantom32, 64:r.phantom64, 128:r.phantom128, 256:r.phantom}
 
-theta = np.linspace(0, np.pi, n_ang, False)
+theta = np.linspace(0, np.pi/2, n_ang, False)
 
 def Af(f):
     # Define the forward projection with given number of angles
@@ -57,7 +56,7 @@ def norm_mat(A, At, f_shape):
 
 def tik_primal_dual(meas, f_shape, alpha, show_prog=False):
     '''
-    Computes the least squares minimizer with respect to f  to the problem 1/2 norm(Af - g)^2, where norm denotes 
+    Computes the least squares minimizer with respect to f  to the problem norm(Af - g)^2 + alpha*norm(f)^2, where norm denotes 
     the L2-norm, using the primal-dual method. In this case A is the radon transform, f is the reconstruction we want to
     obtain and g is the measurement data given by the parameter meas.
     '''
@@ -74,16 +73,19 @@ def tik_primal_dual(meas, f_shape, alpha, show_prog=False):
     fk = np.zeros(f_shape)
     ft = fk
 
-    for i in range(1001):
-        pk = (2*(pk+sigma*(Af(ft) - meas)))/(sigma+2)
+    for i in range(2001):
+        pk = (pk+sigma*(Af(ft) - meas))/(sigma+1)
         f_prev = fk
         fk = (fk-tau*Atf(pk))/(2*alpha*tau+1)
-        fk[fk<0] = 0 #Positivity constraint
+        fk[fk<0]=0
         ft = fk + Theta*(fk-f_prev)
 
         if show_prog:
             if i % 50 == 0:
-                gap = np.linalg.norm(Af(fk)-meas)**2 + alpha*np.linalg.norm(fk)**2 + 1/(4*alpha)*np.linalg.norm(Atf(pk))**2 + np.dot(pk.flatten(),meas.flatten()) +1/4*np.linalg.norm(pk)**2
+                at = -Atf(pk)
+                at[at<0] = 0
+                Gconj = 1/(4*alpha)*np.linalg.norm(at)**2
+                gap = 1/2*np.linalg.norm(Af(fk)-meas)**2 + alpha*np.linalg.norm(fk)**2 + Gconj + np.dot(pk.flatten(),meas.flatten()) +1/2*np.linalg.norm(pk)**2
                 pylab.figure(1)
                 pylab.title("Iteration {} | Dual gap: {:.2f}".format(i, gap))
                 pylab.gray()
@@ -121,7 +123,7 @@ def tik_primal_dual_mat(A, At, meas, f_shape, alpha, show_prog=False):
 
         if show_prog:
             if i % 50 == 0:
-                gap = np.linalg.norm(A.dot(ft)-meas_v)**2 + 1/(4*alpha)*np.linalg.norm(-At.dot(pk))**2 + np.dot(pk,meas_v) +1/4*np.linalg.norm(pk)**2
+                gap = np.linalg.norm(A.dot(ft)-meas_v)**2 + 1/(4)*np.linalg.norm(-At.dot(pk))**2 + np.dot(pk,meas_v) +1/4*np.linalg.norm(pk)**2
                 pylab.figure(2)
                 pylab.title("Iteration {} | Dual gap: {:.2f}".format(i, gap))
                 pylab.gray()
@@ -140,11 +142,11 @@ if __name__ == "__main__":
     #At = np.transpose(A)
 
     # Adding noise
-    noise_level = 0.01
+    noise_level = 0.05
     noisy_data = data + noise_level*np.random.randn(data.shape[0], data.shape[1])
 
     # Compute the reconstruction
-    rec = tik_primal_dual(noisy_data, (n, n), 5, True)
+    rec = tik_primal_dual(noisy_data, (n, n), 0.1, True)
     #rec = tik_primal_dual_mat(A, At, noisy_data, (n,n), 0.01, True)
 
     pylab.gray()
@@ -155,5 +157,6 @@ if __name__ == "__main__":
     pylab.figure(3)
     pylab.plot(np.linspace(0,n,n), rec[n//2,:], 'r', np.linspace(0,n,n), phantom[n][n//2,:], 'b')
 
+    print("Error: {}".format(np.linalg.norm(phantom[n]-rec)))
 
     pylab.show()